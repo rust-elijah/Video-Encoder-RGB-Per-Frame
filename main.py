2507f4b2ee279325b7f1ab395310f023af4d426e
Elijah Rust <erust@C02F83UXMD6R.local>
2022-02-14 14:44:23 -0700

    Renamed filename

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import encoder 
 
 def main():
-    encoder.encode("Big_Buck_Bunny_360_10s_1MB.mp4","Big_Buck_Bunny_360_10s_1MB.mp4",.1,.1,10)
+    encoder.encode("Big_Buck_Bunny_360_10s_1MB","Big_Buck_Bunny_360_10s_1MB.mp4",.1,.1,10)
 
 if __name__== "__main__":
     main()
+
+ 
